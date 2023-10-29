c92b6fd6e8f3e434d03ffb4c5833833ba2c84166
César Rafael <cesarrdev@gmail.com>
2023-10-29 13:05:18 -0300

    feat: Added try catch function to deal with possible errors coming from the external api

--- src/main/java/br/com/digivice/service/DigimonService.java
+++ src/main/java/br/com/digivice/service/DigimonService.java
@@ -1,6 +1,11 @@
 package br.com.digivice.service;
 
 import br.com.digivice.DTO.DigimonDTO;
+import br.com.digivice.exceptions.EntityNotFoundException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +13,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class DigimonService {
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
     String urlExternalApi = "https://digimon-api.vercel.app/api/digimon";
     String auxUrlApi = this.urlExternalApi;
 
@@ -42,7 +49,8 @@ public class DigimonService {
         return this.requestToExternalApi(this.urlExternalApi);
     }
 
-    public List<DigimonDTO> requestToExternalApi(String url){
+    public List<DigimonDTO> requestToExternalApi(String url) {
+        try {
             ResponseEntity<List<DigimonDTO>> response = this.restTemplate.exchange(
                     this.urlExternalApi,
                     HttpMethod.GET,
@@ -54,7 +62,16 @@ public class DigimonService {
             this.urlExternalApi = auxUrlApi;
 
             return response.getBody();
+        } catch(HttpClientErrorException error){
 
+            this.urlExternalApi = auxUrlApi;
+
+            int startIndex = error.getMessage().indexOf("\"ErrorMsg\":\"") + 12;
+            int endIndex = error.getMessage().indexOf("\"}", startIndex);
+            String errorMessage = error.getMessage().substring(startIndex, endIndex);
+
+            throw  new EntityNotFoundException(error.getStatusCode(),errorMessage);
+        }
     }
 
 }
