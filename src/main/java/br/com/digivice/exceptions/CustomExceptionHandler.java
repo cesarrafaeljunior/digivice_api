d5923f446a9b6ac7b34f137fe4f966aca75f2f43
César Rafael <cesarrdev@gmail.com>
2023-10-29 13:02:30 -0300

    Feat: Created a class to reference custom exception classes

--- src/main/java/br/com/digivice/exceptions/CustomExceptionHandler.java
+++ src/main/java/br/com/digivice/exceptions/CustomExceptionHandler.java
@@ -1,23 +1,25 @@
 package br.com.digivice.exceptions;
 
-import org.springframework.http.HttpStatus;
+import br.com.digivice.exceptions.errors.StandardError;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.Instant;
 
 @ControllerAdvice
-public class ExceptionHandler{
+public class CustomExceptionHandler{
 
-    @org.springframework.web.bind.annotation.ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<StandardError> handleEntityNotFoundException(EntityNotFoundException e){
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandardError> handleEntityNotFoundException(EntityNotFoundException e, HttpServletRequest request){
 
         StandardError error = new StandardError();
 
         error.setTimestamp(Instant.now());
         error.setError(e.getMessage());
-        error.setStatus(HttpStatus.NOT_FOUND);
-        error.setPath("teste");
+        error.setStatus(e.getStatusCode());
+        error.setPath(request.getRequestURI());
 
         return ResponseEntity.status(error.getStatus()).body(error);
     }
