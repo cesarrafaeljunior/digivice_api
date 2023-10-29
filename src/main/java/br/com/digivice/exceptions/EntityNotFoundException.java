14e704336b09fe5fbf922867196cc86dbee9a09c
César Rafael <cesarrdev@gmail.com>
2023-10-29 13:03:46 -0300

    feat: Class responsible for extending and handling error exceptions thrown by HttpClientErrorException

--- src/main/java/br/com/digivice/exceptions/EntityNotFoundException.java
+++ src/main/java/br/com/digivice/exceptions/EntityNotFoundException.java
@@ -1,2 +1,27 @@
-package br.com.digivice.exceptions;public class EntityNotFoundException {
+package br.com.digivice.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.client.HttpClientErrorException;
+
+public class EntityNotFoundException extends HttpClientErrorException{
+
+    private final String message;
+    private final HttpStatusCode statusCode;
+    public EntityNotFoundException(HttpStatusCode statusCode, String message){
+        super(HttpStatus.BAD_REQUEST);
+        this.statusCode = statusCode;
+        this.message = message;
+
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public HttpStatusCode getStatusCode() {
+        return statusCode;
+    }
 }
